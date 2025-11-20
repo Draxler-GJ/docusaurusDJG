d5a75848dd605165ec652a22e7df5c3b2e120e0e
Draxler-GJ <danjorgom@alu.edu.gva.es>
2025-11-20 18:28:26 +0100

    modificació configuarció per a vercel

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/Draxler-GJ/Draxler-GJ.github.io',
+  url: 'https://Draxler-GJ.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurusDJG/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
